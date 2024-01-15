75ba7dc4e4e420afd4e93285b77ea6c17aed388d
calilik20 <156676874+calilik20@users.noreply.github.com>
2024-01-15 16:17:19 +0300

    Add files via upload

---
+++ Faktöriyel_bulma.py
@@ -0,0 +1,22 @@
+print("""**************************
+
+Faktöriyel Bulma Programı
+
+
+
+Çıkmak için q ya basınız..
+**************************""")
+
+while True:
+   sayı = input("Sayı:")
+   if (sayı == "q"):
+       print("Program kapanıyor.")
+       break
+   else:
+       sayı = int(sayı)
+
+       faktoriyel = 1
+
+       for i in range(2,sayı+1):
+           faktoriyel *= i
+           print("Faktöriyel ",faktoriyel)
